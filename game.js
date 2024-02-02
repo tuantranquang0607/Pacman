9471402fe219010e028d82162deec7af9729121e
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-02-02 15:52:29 -0400

    15:51-02/02/2024: I finished making Pacman, its moving directions, food, and the scores..

--- game.js
+++ game.js
@@ -3,10 +3,16 @@ const canvasContext = canvas.getContext('2d');
 const pacmanFrames = document.getElementById('animations');
 const ghostFrames = document.getElementById('ghosts');
 
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
+
 let createRect = (x, y, width, height, color) => {
     canvasContext.fillStyle = color;
     canvasContext.fillRect(x, y, width, height);
 };
+
 let fps = 30;
 
 let oneBlockSize = 20;
@@ -18,6 +24,10 @@ let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 
 let wallInnerColor = 'black';
+
+let foodColor = 'white';
+
+let score = 0;
 
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -73,12 +83,38 @@ let gameLoop = () => {
 };
 
 let update = () => {
+    pacman.moveProcess();
+    pacman.eat();
+};
 
+let drawFoods = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] == 2) {
+                createRect(
+                    j * oneBlockSize + oneBlockSize / 3,
+                    i * oneBlockSize + oneBlockSize / 3,
+                    oneBlockSize / 3,
+                    oneBlockSize / 3,
+                    foodColor
+                );
+            };
+        };
+    };
+};
+
+let drawScore = () => {
+    canvasContext.font = '20px Emulogic';
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillText('Score: ' + score, 0, oneBlockSize * (map.length + 1) + 10);
 };
 
 let draw = () => {
     createRect(0, 0, canvas.width, canvas.height, 'black');
     drawWalls();
+    drawFoods();
+    pacman.draw();
+    drawScore();
 };
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
@@ -139,3 +175,31 @@ let drawWalls = () => {
     };
 };
 
+let createNewPacman = () => {
+    pacman = new Pacman(
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize / 5
+    );
+};
+
+createNewPacman();
+gameLoop();
+
+window.addEventListener('keydown', (e) => {
+    let k = e.keyCode;
+
+    setTimeout(() => {
+        if (k == 37 || k == 65) {
+            pacman.nextDirection = DIRECTION_LEFT;
+        } else if (k == 38 || k == 87) {
+            pacman.nextDirection = DIRECTION_UP;
+        } else if (k == 39 || k == 68) {
+            pacman.nextDirection = DIRECTION_RIGHT;
+        } else if (k == 40 || k == 83) {
+            pacman.nextDirection = DIRECTION_BOTTOM;
+        };
+    }, 1);
+});
